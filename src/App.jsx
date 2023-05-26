baf8f2595d9417046d98f1dd03b36b67ed99835c
JulianIgnacio <montenegrojulianig@gmail.com>
2023-05-25 21:03:56 -0300

    agregando estructura inicial react

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Titulos from './Components/Titulos';
+
+const App= () => {
+
+  return (
+    <>
+    <Titulos/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Titulos.jsx
@@ -0,0 +1,11 @@
+
+const Titulos = () => {
+    return(
+        <section>
+            <h1>Primera App React</h1>
+            <h2>Subtitulo</h2>
+        </section>
+    );
+};
+
+export default Titulos;
