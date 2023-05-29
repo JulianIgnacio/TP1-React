3cf9207363a78f5a66e9afaa4ae69e4676130f3a
JulianIgnacio <montenegrojulianig@gmail.com>
2023-05-28 23:21:34 -0300

    agregando estructura inicial al maquetado

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import { Container } from 'react-bootstrap';
 import './App.css'
-import Mensaje from './Components/Titulos';
+import ListaDeTareas from './Components/ListaDeTareas';
 
 const App= () => {
 
   return (
     <>
-    <Mensaje/>
+    <Container className='my-5 mainPage'>
+    <h1 className='display-4 text-center text-light'>Lista de Tareas</h1>
+    <hr className='text-light'/>
+    <ListaDeTareas/>
+    </Container>
+    <footer className="bg-dark text-light text-center py-4">
+        <p>&copy; Todos los derechos reservados</p>
+      </footer>
     </>
   );
 }
---
+++ src/Components/ListaDeTareas.jsx
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const ListaDeTareas = () => {
+
+    return(
+        <section>
+        </section>
+    );
+};
+
+export default ListaDeTareas;
