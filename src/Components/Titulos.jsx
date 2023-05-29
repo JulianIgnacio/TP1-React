ba2ab918e457c4d7c92d080d622d715880242954
JulianIgnacio <montenegrojulianig@gmail.com>
2023-05-28 21:55:26 -0300

    agregando detalles ejercicio 3

--- src/Components/Titulos.jsx
+++ src/Components/Titulos.jsx
@@ -1,9 +1,12 @@
-
-const Mensaje = ({ nuevoMensaje }) => {
+import { useState } from "react";
+const Mensaje = () => {
+    
+    const [nuevoMensaje,setMessaje] = useState (0);
 
     return(
         <section>
-            <h1>{ nuevoMensaje }</h1>
+            <h1>{nuevoMensaje}</h1>
+            <button onClick={() => {setMessaje(nuevoMensaje+"from changed state")}}>click me!</button>
         </section>
     );
 };
